89275be1080120452ca28658037d2e12ee055661
Arkaitz Garro <arkaitz.garro@gmail.com>
2017-07-07 14:42:40 +0200

    Rewrite specs for Debounce module

--- spec/tests.js
+++ spec/tests.js
@@ -1,38 +1,38 @@
-import test from 'ava';
 import Debounce from '../index';
 import sinon from 'sinon';
 
-test('should return function', t => {
-  t.is(typeof Debounce(sinon.stub(), 100), 'function');
-});
+describe('Debounce module', () => {
+  it('hould return function', () => {
+    expect(typeof Debounce(sinon.stub(), 100)).toEqual('function');
+  });
 
-//TODO Assertations
-test('should debounce function if called multiple times', async t => {
-  var counter = 0;
-  var test = Debounce(() => counter++, 50);
+  it('hould debounce function if called multiple times', async () => {
+    let counter = 0;
+    const test = Debounce(() => counter++, 50);
 
-  test();
-  test();
-  test();
-  test();
+    test();
+    test();
+    test();
+    test();
 
-  await new Promise(resolve => setTimeout(resolve, 300));
+    await new Promise(resolve => setTimeout(resolve, 300));
 
-  t.is(counter, 1);
-});
+    expect(counter).toEqual(1);
+  });
 
-test('should default the debounce to 1ms if no interval is given', async t => {
-  var counter = 0;
-  var test = Debounce(() => counter++);
+  it('hould default the debounce to 1ms if no interval is given', async () => {
+    let counter = 0;
+    const test = Debounce(() => counter++);
 
-  test();
-  test();
-  await new Promise(resolve => setTimeout(resolve, 5));
+    test();
+    test();
+    await new Promise(resolve => setTimeout(resolve, 5));
 
-  test();
-  test();
+    test();
+    test();
 
-  await new Promise(resolve => setTimeout(resolve, 300));
+    await new Promise(resolve => setTimeout(resolve, 300));
 
-  t.is(counter, 2);
+    expect(counter).toEqual(2);
+  });
 });

e8e54c627fd2c0aa4a9f16457fd5f276c7e377d2
Arkaitz Garro <arkaitz.garro@gmail.com>
2017-07-21 12:43:13 +0200

    Use Jest time fakers

--- spec/tests.js
+++ spec/tests.js
@@ -2,11 +2,13 @@ import Debounce from '../index';
 import sinon from 'sinon';
 
 describe('Debounce module', () => {
+  jest.useFakeTimers();
+
   it('should return function', () => {
     expect(typeof Debounce(sinon.stub(), 100)).toEqual('function');
   });
 
-  it('should debounce function if called multiple times', async () => {
+  it('should debounce function if called multiple times', () => {
     let counter = 0;
     const test = Debounce(() => counter++, 50);
 
@@ -15,24 +17,25 @@ describe('Debounce module', () => {
     test();
     test();
 
-    await new Promise(resolve => setTimeout(resolve, 300));
-
+    expect(counter).toEqual(0);
+    jest.runAllTimers();
     expect(counter).toEqual(1);
   });
 
-  it('should default the debounce to 1ms if no interval is given', async () => {
+  it('should default the debounce to 1ms if no interval is given', () => {
     let counter = 0;
     const test = Debounce(() => counter++);
 
     test();
     test();
-    await new Promise(resolve => setTimeout(resolve, 5));
+
+    jest.runAllTimers();
+    expect(counter).toEqual(1);
 
     test();
     test();
 
-    await new Promise(resolve => setTimeout(resolve, 300));
-
+    jest.runAllTimers();
     expect(counter).toEqual(2);
   });
 });

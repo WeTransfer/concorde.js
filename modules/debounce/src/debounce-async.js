ca94a2f40f064bb7ca2a0a7ca4fe51fab3455fe7
Arkaitz Garro <arkaitz.garro@gmail.com>
2018-02-05 11:15:02 +0100

    fix(docs): add docs for debounceAsync function

--- modules/debounce/src/debounce-async.js
+++ modules/debounce/src/debounce-async.js
@@ -1,3 +1,31 @@
+/**
+ * Debounce async function lets you debounce asynchronous function executions.
+ * @module Debounce
+ * @since 1.0.0
+ */
+
+/**
+ * Creates a debounced function that delays invoking the provided function until after `wait` milliseconds
+ * have elapsed since the last time the debounced function was invoked.
+ * @since 1.0.0
+ * @function debounceAsync
+ * @param {Function} fn The original function to debounce.
+ * @param {number} [wait=0] The number of milliseconds to delay.
+ * @param {Object} [options = { cancelObj = { name: 'debounceAsync', message: 'cancelled debounce' } }] Optional cancel object that will be passed as rejection argument.
+ * @example
+ *
+ * import { debounceAsync } from '@wetransfer/concorde-debounce';
+ *
+ * let counter = 0;
+ * const debounced = debounceAsync(async (increment = 1) => {
+ *   counter += increment;
+ *   return Promise.resolve(counter);
+ * });
+ *
+ * debounced(1).catch(() => console.error('rejected 1'));
+ * debounced(2).catch(() => console.error('rejected 2'));
+ * debounced(3).then(() => console.log(counter));  // => 3
+ */
 export function debounceAsync(
   func,
   wait = 0,

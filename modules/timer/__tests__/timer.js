a06f210e6a5fd87bddf04c8c3d225974db95a66c
Sebass van Boxel <sebass@wetransfer.com>
2018-04-13 08:52:35 +0200

    feat: implement timer module (#108)
    * feat: Implement timing module
    
    * fix(docs): Styling jsdoc
    
    * fix(docs): add docs for Timer class
    
    * Refactored specs
    
    * fix: timer specs mock date
    
    * fix(specs): set timezone

---
+++ modules/timer/__tests__/timer.js
@@ -0,0 +1,198 @@
+import Timer from '../index';
+
+describe('Timer module', () => {
+  const RealDate = Date;
+
+  function mockDate(isoDate) {
+    global.Date = class extends RealDate {
+      constructor() {
+        super();
+        return new RealDate(isoDate);
+      }
+    };
+  }
+
+  afterEach(() => {
+    // Resetting the Date object
+    global.Date = RealDate;
+  });
+
+  describe('timer constructor', () => {
+    let spy;
+    let timer;
+    beforeEach(() => {
+      mockDate('Friday, 1 January 2016 00:23:45');
+    });
+
+    describe('no callback is given', () => {
+      it('should throw an error if no callback is given', () => {
+        expect(() => new Timer(100)).toThrow('ValidCallbackMissing');
+      });
+    });
+
+    describe('callback and delay are given', () => {
+      let startSpy;
+      beforeEach(() => {
+        spy = jest.fn();
+        startSpy = jest.spyOn(Timer.prototype, 'start');
+        timer = new Timer(100, spy);
+      });
+
+      it('should set the right properties and call start', () => {
+        expect(timer).toMatchObject({
+          remaining: 100,
+          delay: 100,
+          callback: spy,
+          paused: false,
+          time: 1451604225000
+        });
+      });
+
+      it('should call the start method', () => {
+        expect(startSpy).toHaveBeenCalled();
+      });
+    });
+
+    describe('callback is given, delay not', () => {
+      it('should set the default delay (0)', () => {
+        timer = new Timer(undefined, spy);
+        expect(timer.remaining).toBe(0);
+      });
+    });
+  });
+
+  describe('start method', () => {
+    it('should set the setTimeout method to the id property', () => {
+      jest.useFakeTimers();
+      const callbackSpy = jest.fn();
+      const startStub = jest.spyOn(Timer.prototype, 'start');
+      const timer = new Timer(10, callbackSpy);
+      startStub.mockReset();
+
+      timer.start();
+      jest.runAllTimers();
+      expect(callbackSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('stop method', () => {
+    it('should call timer.stop with the id-property', () => {
+      const callbackSpy = jest.fn();
+      const startStub = jest.spyOn(Timer.prototype, 'start');
+      const timer = new Timer(10, callbackSpy);
+      startStub.mockReset();
+
+      timer.id = 5;
+      const clearTimeoutSpy = jest.spyOn(global, 'clearTimeout');
+      timer.stop();
+      expect(clearTimeoutSpy).toBeCalledWith(5);
+    });
+  });
+
+  describe('pause method', () => {
+    let timer;
+    let callbackSpy;
+    let startSpy;
+
+    beforeEach(() => {
+      callbackSpy = jest.fn();
+      startSpy = jest.spyOn(Timer.prototype, 'start');
+      timer = new Timer(10, callbackSpy);
+      startSpy.mockReset();
+    });
+
+    describe('timer is paused', () => {
+      beforeEach(() => {
+        timer.paused = true;
+      });
+
+      it('should return undefined', () => {
+        expect(timer.pause()).toBeUndefined();
+      });
+    });
+
+    describe('timer is NOT paused', () => {
+      let stopSpy;
+      beforeEach(() => {
+        timer.paused = false;
+        timer.time = 1451701625105;
+        mockDate('Friday, 2 January 2016 03:23:45');
+        stopSpy = jest.spyOn(timer, 'stop');
+        timer.pause();
+      });
+
+      it('should set the paused property to true', () => {
+        expect(timer.paused).toBe(true);
+      });
+
+      it('should call the stop method', () => {
+        expect(stopSpy).toHaveBeenCalledTimes(1);
+      });
+
+      it('should set the remaining time', () => {
+        expect(timer.remaining).toEqual(200115);
+      });
+    });
+  });
+
+  describe('resume method', () => {
+    let timer;
+    let callbackSpy;
+    let startSpy;
+
+    beforeEach(() => {
+      callbackSpy = jest.fn();
+      timer = new Timer(10, callbackSpy);
+      startSpy = jest.spyOn(timer, 'start');
+      startSpy.mockReset();
+
+      mockDate('Friday, 2 January 2016 03:23:45');
+      timer.paused = true;
+      timer.time = 10;
+      timer.resume();
+    });
+
+    it('should set the paused property to false', () => {
+      expect(timer.paused).toBe(false);
+    });
+
+    it('set the time property to the current timestamp', () => {
+      expect(timer.time).toEqual(1451701425000);
+    });
+
+    it('should call the startSpy', () => {
+      expect(startSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('remainingTime getter', () => {
+    let timer;
+    beforeEach(() => {
+      timer = new Timer(10, () => 'foo');
+    });
+
+    describe('timer is paused', () => {
+      beforeEach(() => {
+        timer.paused = true;
+        timer.remaining = 1476278108301;
+      });
+
+      it('should return the right time', () => {
+        expect(timer.remainingTime).toEqual(1476278108.301);
+      });
+    });
+
+    describe('timer is NOT paused', () => {
+      beforeEach(() => {
+        mockDate('Friday, 2 January 2016 03:23:45');
+        timer.paused = false;
+        timer.time = 1451607825105;
+        timer.remaining = 1476278108301;
+      });
+
+      it('should return the right time', () => {
+        expect(timer.remainingTime).toEqual(1476184508.406);
+      });
+    });
+  });
+});
---
+++ modules/timer/src/timer.js
@@ -0,0 +1,183 @@
+/**
+ * Timer module. Useful functions to deal with timing in javascript.
+ * @module Timer
+ * @since 1.0.0
+ */
+export default class Timer {
+  /**
+   * Store the properties of the timer and start it.
+   * @constructor
+   * @since 1.0.0
+   * @param {Integer} [delay=0] The time, in milliseconds, the timer should wait before the specified function or code is executed. The actual delay may be longer than intended; JavaScript timing is notoriously bad.
+   * @param {Function} callback A function to be executed after the timer expires.
+   * @returns {Timer} The timer object
+   * @example
+   * import Timer from '@wetransfer/concorde-timer';
+   *
+   * // (T)
+   * const timedFunction = () => {
+   *    console.log('ping!')
+   * }
+   *
+   * const stopwatch = new Timer(10000, timedFunction);
+   * // => Timer {remaining: 10000, callback: ƒ, paused: false, delay: 10, time: 1523436642013}
+   *
+   * // (T+5 seconds)
+   * stopwatch
+   * // => Timer {remaining: 5000, callback: ƒ, paused: false, delay: 10, time: 1523436647000}
+   *
+   * // (T+10 seconds)
+   * // 'ping!'
+   */
+  constructor(delay = 0, callback) {
+    // Callback MUST be a function
+    if (typeof callback !== 'function') {
+      throw 'ValidCallbackMissing';
+    }
+
+    this.remaining = delay;
+    this.callback = callback;
+    this.paused = false;
+    this.delay = delay;
+    this.time = +new Date();
+
+    this.start();
+  }
+
+  /**
+   * (Re)starts the Timer by creating a new timeout
+   * @since 1.0.0
+   * @function start
+   * @example
+   * import Timer from '@wetransfer/concorde-timer';
+   *
+   * // (T)
+   * const timedFunction = () => {
+   *    console.log('pong!');
+   * }
+   *
+   * const stopwatch = new Timer(10000, timedFunction);
+   *
+   * // (T+2 seconds)
+   * stopwatch.pause(); (see Timer.pause)
+   *
+   * // (T+10 seconds)
+   * stopwatch.start();
+   *
+   * // (T+18 seconds)
+   * // 'pong!'
+   */
+  start() {
+    // Basically it's a simple timeOut
+    this.id = setTimeout(this.callback, this.remaining);
+  }
+
+  /**
+   * Stops and clears the Timer
+   * @since 1.0.0
+   * @function stop
+   * @example
+   * import Timer from '@wetransfer/concorde-timer';
+   *
+   * // (T)
+   * const timedFunction = () => {
+   *    console.log('pung!');
+   * }
+   *
+   * const stopwatch = new Timer(10000, timedFunction);
+   *
+   * // (T+2 seconds)
+   * stopwatch.stop();
+   *
+   * // (void)
+   */
+  stop() {
+    // That we can stop
+    clearTimeout(this.id);
+  }
+
+  /**
+   * Pauses the current Timer
+   * @since 1.0.0
+   * @function pause
+   * @example
+   * import Timer from '@wetransfer/concorde-timer';
+   *
+   * // (T)
+   * const timedFunction = () => {
+   *    console.log('peng!');
+   * }
+   *
+   * const stopwatch = new Timer(10000, timedFunction);
+   *
+   * // (T+2 seconds)
+   * stopwatch.pause();
+   *
+   * // (T+10 seconds)
+   * stopwatch.resume(); (see Timer.resume)
+   *
+   * // (T+18 seconds)
+   * // 'peng!'
+   */
+  pause() {
+    if (this.paused) {
+      return;
+    }
+    // Stop and keep track of the current time remaining
+    this.paused = true;
+    this.stop();
+    this.remaining -= +new Date() - this.time;
+  }
+
+  /**
+   * Resumes the current Timer
+   * @since 1.0.0
+   * @function pause
+   * @example
+   * import Timer from '@wetransfer/concorde-timer';
+   *
+   * // (T)
+   * const timedFunction = () => {
+   *    console.log('pang!');
+   * }
+   *
+   * const stopwatch = new Timer(10000, timedFunction);
+   *
+   * // (T+2 seconds)
+   * stopwatch.pause(); (see Timer.pause)
+   *
+   * // (T+10 seconds)
+   * stopwatch.resume();
+   *
+   * // (T+18 seconds)
+   * // 'pang!'
+   */
+  resume() {
+    // Restart with the time remaining
+    this.paused = false;
+    this.time = +new Date();
+    this.start();
+  }
+
+  /**
+   * @since 1.0.0
+   * @member {Integer} remainingTime The time that is remaining for the current Timer.
+   * @example
+   * import Timer from '@wetransfer/concorde-timer';
+   *
+   * // (T)
+   * const stopwatch = new Timer(10000, () => 'foo'));
+   *
+   * // (T+2 seconds)
+   * stopwatch.remainingTime
+   * // 8000
+   */
+  get remainingTime() {
+    // And calculate the time remaining when paused or running.
+    if (this.paused) {
+      return Math.max(0, this.remaining / 1000);
+    }
+
+    return Math.max(0, (this.remaining - (+new Date() - this.time)) / 1000);
+  }
+}

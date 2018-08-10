d04cd3728acef6fcbd66a74a9c54e931bbbd94b4
Arkaitz Garro <arkaitz.garro@gmail.com>
2018-08-10 13:46:00 +0200

    chore: fix trailing commas

--- modules/timer/jest.config.js
+++ modules/timer/jest.config.js
@@ -1,5 +1,5 @@
 const config = require('../../scripts/jest/default-config');
 
 module.exports = config({
-  setupFiles: ['../../scripts/jest/setup-environment.js']
+  setupFiles: ['../../scripts/jest/setup-environment.js'],
 });

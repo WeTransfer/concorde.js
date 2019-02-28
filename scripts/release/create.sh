8f9858169354dd580ac0217eb1b55b886b27a6e5
Arkaitz Garro <arkaitz.garro@gmail.com>
2019-02-28 08:14:59 +0100

    chore: simplify release script

--- scripts/release/create.sh
+++ scripts/release/create.sh
@@ -4,18 +4,7 @@ set -e
 
 # Checkout master branch
 echo "[GIT] Checking out master...."
-# git checkout master
-
-# Pull the latest code from origin
-echo "[GIT] Pulling latest changes..."
-# git pull --rebase origin master
-
-# Create a new branch to release from
-echo "[GIT] Creating a new branch..."
-date=`date +r%Y-%m-%d-%s`
-git checkout -b release/$date
+git checkout master
 
 node_modules/.bin/lerna bootstrap
-node_modules/.bin/lerna publish --conventional-commits
-
-git push --no-verify --follow-tags --set-upstream origin release/$date
+node_modules/.bin/lerna publish --conventional-commits --github-release
